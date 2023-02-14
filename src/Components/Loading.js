062028b7d35e45996f8be553c5d8564c2d14bced
BaronCoding <mikegbaron@gmail.com>
2023-02-14 09:08:08 +0200

    Bug fix

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Loading() {
   return (
     <div className='min-h-screen bg-blueGrey-700 flex justify-center items-center text-4xl '>
-        <div className='font-header font-bold animate-colorChange'>Mike G Baron</div>
+        <div className='font-header font-bold animate-colorChange'>Mike G Baron.</div>
     </div>
   )
 }
