4997d61a966014f892491f590d7d13de24bc1891
BaronCoding <mikegbaron@gmail.com>
2023-03-01 21:16:14 +0200

    updated contact

--- src/Components/Thanks.js
+++ src/Components/Thanks.js
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 
 export default function Thanks() {
   return (
-    <div className='z-10'>
-      <h1>Thank you!</h1> 
-      <Link to='/'><h3>Let's go BACK</h3></Link>
+    <div className='bg-blueGrey-100 text-blueGrey-500
+    dark:bg-blueGrey-700 dark:text-blueGrey-100 h-full flex items-center justify-center'>
+      <div>
+        <div className='text-4xl font-black m-10'>Thank you!</div>
+        <Link to='/'><div className='text-2xl m-10'>Let's go BACK</div></Link>
+      </div>
+      
     </div>
   )
 }
